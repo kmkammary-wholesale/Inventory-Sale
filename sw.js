0047102304c3527297504a68ba0b750040512954
kmkammary-wholesale <kmkammary@gmail.com>
2026-09-01 21:38:25 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME="inventory-product-sale-v5-7-6-1-icon-fix";
-const ASSETS=["./","./index.html?v=5761","./manifest.json?v=5761","./icon-192.png?v=5761","./icon-512.png?v=5761"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,x));return r;}).catch(()=>caches.match(e.request)));});
