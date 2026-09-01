24866975f1447531153ea365a215f6377b71c11d
kmkammary-wholesale <kmkammary@gmail.com>
2026-09-01 20:23:26 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME="inventory-product-sale-v5-7-6-product-supplier";
-const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,x));return r;}).catch(()=>caches.match(e.request)));});
