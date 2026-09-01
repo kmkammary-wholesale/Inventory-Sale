962d90b137769cfac7dc21febf7f0a86c455e60f
kmkammary-wholesale <kmkammary@gmail.com>
2026-09-01 10:50:08 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME="wholesale-v5-7-3-cache";
-const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))));self.clients.claim();});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
